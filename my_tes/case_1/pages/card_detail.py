1fb47cab90f8a08b3c85121c7276ac4122250eb1
AntLinux <anton-r@live.com>
2021-04-02 16:45:17 +0300

    creating the class for the card

--- my_tes/case_1/pages/card_detail.py
+++ my_tes/case_1/pages/card_detail.py
@@ -1,6 +1,8 @@
 from selenium.webdriver.common.by import By
 from selenium.webdriver.remote.webelement import WebElement
+from typing import Tuple
 
+from my_tes.case_1.models.card import Card
 from my_tes.case_1.pages.common.base import PageBase
 
 
@@ -28,4 +30,14 @@ class CardDetail(PageBase):
         super(CardDetail, self).__init__(driver)
         self.map = CardDetailMap(driver)
 
+    def get_card_type_and_arena(self) -> Tuple[str, int]:
+        # will get :: 'Troop, Arena 8'
+        type, arena = self.map.card_type.text.split(", ")
+        arena = int(arena.split(' ')[-1])
+        return type, arena
 
+    def get_base_card(self) -> Card:
+        type, arena = self.get_card_type_and_arena()
+        name = self.map.card_name.text
+        rarity = self.map.cart_rarity.text.split('\n')[1]  # 'word\nword'
+        return Card(name, type, arena, rarity)
