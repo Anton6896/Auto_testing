35f01a6079f636d93950ee1bd2f77458a1098109
AntLinux <anton-r@live.com>
2021-04-03 16:46:50 +0300

    creating the class for the card

--- my_tes/case_3/pages/page_simple_form.py
+++ my_tes/case_3/pages/page_simple_form.py
@@ -16,6 +16,18 @@ class SimpleFormPageMap:
     def single_field_output_msg(self) -> WebElement:
         return self._driver.find_element_by_id('display')
 
+    def two_inputs_field_a(self) -> WebElement:
+        return self._driver.find_element_by_id('sum1')
+
+    def two_inputs_field_b(self) -> WebElement:
+        return self._driver.find_element_by_id('sum2')
+
+    def two_inputs_btn_submit(self) -> WebElement:
+        return self._driver.find_element_by_xpath('//*[@id="gettotal"]/button')
+
+    def two_inputs_output(self) -> WebElement:
+        return self._driver.find_element_by_id('displayvalue')
+
 
 class SimpleForm(PageInCommon):
     def __init__(self, driver):
@@ -36,3 +48,25 @@ class SimpleForm(PageInCommon):
 
     def get_output_simple_form(self):
         return self.map.single_field_output_msg().text
+
+
+class TwoInputsForm(PageInCommon):
+    def __init__(self, driver):
+        super().__init__(driver)
+        self.map = SimpleFormPageMap(driver)
+
+    def goto_simple_form(self):
+        self.header_navbar.goto_simple_form_demo()
+        return self
+
+    def set_values(self, a: int, b: int):
+        self.map.two_inputs_field_a().send_keys(a)
+        self.map.two_inputs_field_b().send_keys(b)
+        return self
+
+    def submit(self):
+        self.map.two_inputs_btn_submit().click()
+        return self
+
+    def get_output(self):
+        return self.map.two_inputs_output().text
--- my_tes/case_3/test_case_3.py
+++ my_tes/case_3/test_case_3.py
@@ -1,7 +1,7 @@
 from selenium import webdriver
 from pathlib import Path
 
-from my_tes.case_3.pages.page_simple_form import SimpleForm
+from my_tes.case_3.pages.page_simple_form import SimpleForm, TwoInputsForm
 
 BASE_DIR = Path(__file__).resolve().parent.parent
 CHROMEDRIVER = BASE_DIR.joinpath('chromedriver')
@@ -10,10 +10,21 @@ CHROMEDRIVER = BASE_DIR.joinpath('chromedriver')
 def test_simple_form():
     driver = webdriver.Chrome(CHROMEDRIVER)
     driver.get('https://www.seleniumeasy.com/test/')
-    form = SimpleForm(driver)
-    t_value = form.goto_simple_form() \
+    form_lnk = SimpleForm(driver)
+    t_value = form_lnk.goto_simple_form() \
         .set_value_simple_form('valTest') \
         .submit_simple_form() \
         .get_output_simple_form()
 
     assert t_value == 'valTest'
+    driver.close()
+
+
+def test_two_inputs():
+    driver = webdriver.Chrome(CHROMEDRIVER)
+    driver.get('https://www.seleniumeasy.com/test/')
+    """ must be an integers ::  output = (a+b)"""
+    form_link = TwoInputsForm(driver)
+    t_value = form_link.goto_simple_form().set_values(2, 2).submit().get_output()
+    assert t_value == '4'
+    driver.close()
